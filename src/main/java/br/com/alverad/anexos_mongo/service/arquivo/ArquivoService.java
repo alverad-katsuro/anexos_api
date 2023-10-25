713036a4d2c3fd5343047e7bd9804e66ed3da1cb
Alfredo Gabriel <amgabriel1@hotmail.com>
2023-10-24 23:24:05 -0300

    feat: integração sentry admin panel heureka

---
+++ src/main/java/br/com/alverad/anexos_mongo/service/arquivo/ArquivoService.java
@@ -0,0 +1,30 @@
+package br.com.alverad.anexos_mongo.service.arquivo;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.alverad.anexos_mongo.dto.arquivo.ArquivoDTO;
+import br.com.alverad.anexos_mongo.exceptions.NotFoundException;
+
+public interface ArquivoService {
+
+	public ObjectId salvarArquivo(ArquivoDTO arquivo)
+			throws IOException;
+
+	public ObjectId salvarArquivo(ObjectId id, Optional<String> nome, MultipartFile arquivo)
+			throws IOException;
+
+	public ObjectId salvarArquivo(Optional<String> nome, MultipartFile arquivo)
+			throws IOException;
+
+	public ObjectId salvarArquivo(MultipartFile arquivo)
+			throws IOException;
+
+	public GridFsResource recuperarArquivo(ObjectId id)
+			throws IllegalArgumentException, SecurityException, NotFoundException;
+
+}
